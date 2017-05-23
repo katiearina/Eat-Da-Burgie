b4cd5cb4b1161de12c1373e2fa8fcfc3e3831e38
Katie <katiearina@gmail.com>
2017-05-22 20:04:45 -0400

    :lipstick::trident::white_flower: Setting up JawsDB connection.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,23 @@
 // Dependencies
 var mysql = require("mysql");
+var connection;
 // =============================================================
 
 // Set up MySQL connection
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
 });
+};
+
+
 
 // Make connection
 connection.connect(function(err) {
